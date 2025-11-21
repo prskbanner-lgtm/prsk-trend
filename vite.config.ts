2e8dead23ab909839235fd935f23bc918edd2a66
prskbanner-lgtm <prskbanner@gmail.com>
2025-11-21 14:37:10 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   // 公開先が https://USERNAME.github.io/REPO_NAME/ の場合は以下を置き換えてください:
-  base: '/REPO_NAME/',
+  base: '/prsk-trend/',
   plugins: [react()]
 })
