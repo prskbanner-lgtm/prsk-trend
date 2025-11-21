ee51ddea96ac65a17218c25007286db7c2fa0b43
prskbanner-lgtm <prskbanner@gmail.com>
2025-11-21 15:54:51 +0900

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  // 公開先が https://USERNAME.github.io/REPO_NAME/ の場合は以下を置き換えてください:
-  base: '/prsk-trend/',
-  plugins: [react()]
-})
